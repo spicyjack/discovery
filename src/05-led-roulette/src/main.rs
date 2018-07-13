490271c9fcf420e7009870f7957049ede499fe0c
Brian Manning <brian@xaoc.org>
2018-07-12 20:43:32 -0700

    05-led-roulette/src/main.rs: display LEDs in "opposite pairs"

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,28 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Delay, Leds};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let half_period = 500_u16;
+    let micro_period = 50_u16;
+
+    loop {
+      for idx in 0..4 {
+         let next = (idx + 1) % 4;
+
+         leds[idx].on();
+         delay.delay_ms(micro_period);
+         leds[idx + 4].on();
+         delay.delay_ms(half_period);
+
+         leds[idx].off();
+         delay.delay_ms(micro_period);
+         leds[idx + 4].off();
+         delay.delay_ms(half_period);
+      }
+   }
 }

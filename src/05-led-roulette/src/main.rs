f1f1e0b3160eee0b7eb8f570874844af141b1705
Brian Manning <brian@xaoc.org>
2019-06-05 12:02:58 -0700

    05-led-roulette/src/main.rs: added back 'opposite pair' code

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -9,15 +9,19 @@ fn main() -> ! {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
     let half_period = 500_u16;
-    let micro_period = 50_u16;
+    //let micro_period = 50_u16;
 
     loop {
-      for idx in 0..8 {
-         let next = (idx + 1) % 8;
+      for idx in 0..4 {
+         // set up the opposite of the pair
+         //let next = (idx + 1) % 4;
+
          leds[idx].on();
-         delay.delay_ms(micro_period);
+         leds[idx + 4].on();
+         delay.delay_ms(half_period);
+
          leds[idx].off();
-         delay.delay_ms(micro_period);
+         leds[idx + 4].off();
       }
    }
 }
